98fc09c73278544b8938ffe3ed6419d942c03e79
Nirmal Shah <nir.shah@outlook.com>
2021-11-25 21:13:08 +0530

    miner change

--- js/main.js
+++ js/main.js
@@ -6,15 +6,17 @@
 
 })
 
+// Production Link
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzhcgXfFbr-eJVlXEP2tcxmR-maWvcwIIcflq0BeIM27fXi8KUbYrXA9ArGGaBZKiaR0A/exec'
-  const form = document.forms['register-form']
+
+// Test Link
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbzTImkVAsDTUlbrpeHHUCttlHs_nEEBuZByWHsw6iLMZvKN8dqtLB__HEUmcXDMc8fy/exec'
+const form = document.forms['register-form']
 
   form.addEventListener('submit', e => {
-
     e.preventDefault()
-
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+      .then(response => alert("ફોર્મ સફળતાપૂર્વક રજીસ્ટર થયું..! અમે ટૂંક સમયમાં તમારો સંપર્ક કરીશું..!"))
       .catch(error => console.error('Error!', error.message))
 
   });(jQuery);
