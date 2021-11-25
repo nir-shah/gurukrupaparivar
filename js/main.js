c0645d1e51e701dea024cea7696c9001cef93ba8
Nirmal Shah <nir.shah@outlook.com>
2021-11-25 09:26:33 +0530

    Minner Changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 (function($) {
 
   $('#reset').on('click', function(){
-      $('#register-form').reset();
+      // $('#register-form').reset();
+      document.location.reload(true);
   });
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzhcgXfFbr-eJVlXEP2tcxmR-maWvcwIIcflq0BeIM27fXi8KUbYrXA9ArGGaBZKiaR0A/exec'
   const form = document.forms['register-form']
 
   form.addEventListener('submit', e => {
+
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => console.log('Success!', response), )
       .catch(error => console.error('Error!', error.message))
   });
 
