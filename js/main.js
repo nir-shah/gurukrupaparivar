c17b71181e3efe397561cf1dab7284af6a6cbe86
Nirmal Shah <nir.shah@outlook.com>
2021-11-25 15:47:21 +0530

    issue fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (function($) {
 
   $('#reset').on('click', function(){
-      // $('#register-form').reset();
       document.location.reload(true);
   });
+
+})
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzhcgXfFbr-eJVlXEP2tcxmR-maWvcwIIcflq0BeIM27fXi8KUbYrXA9ArGGaBZKiaR0A/exec'
   const form = document.forms['register-form']
@@ -11,9 +12,9 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbzhcgXfFbr-eJVlXEP2tc
   form.addEventListener('submit', e => {
 
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response), )
-      .catch(error => console.error('Error!', error.message))
-  });
 
-})(jQuery);
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+
+  });(jQuery);
