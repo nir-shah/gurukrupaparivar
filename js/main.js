9bc4afb1c6c2a8bee091698479b23745d0c1aa8e
Nirmal Shah <nir.shah@outlook.com>
2021-12-03 15:34:20 +0530

    disable submit on click, unable after alert ok.

--- js/main.js
+++ js/main.js
@@ -3,20 +3,21 @@
   $('#reset').on('click', function(){
       document.location.reload(true);
   });
+});
 
-})
-
-// Production Link
+// Sedding registered form data to google spreadsheet
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzhcgXfFbr-eJVlXEP2tcxmR-maWvcwIIcflq0BeIM27fXi8KUbYrXA9ArGGaBZKiaR0A/exec'
-
-// Test Link
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbzTImkVAsDTUlbrpeHHUCttlHs_nEEBuZByWHsw6iLMZvKN8dqtLB__HEUmcXDMc8fy/exec'
 const form = document.forms['register-form']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-      .then(response => alert("ફોર્મ સફળતાપૂર્વક રજીસ્ટર થયું..! અમે ટૂંક સમયમાં તમારો સંપર્ક કરીશું..!"))
-      .catch(error => console.error('Error!', error.message))
 
-  });(jQuery);
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+    .then((response) => {
+      if(!alert("ફોર્મ સફળતાપૂર્વક રજીસ્ટર થયું..! અમે ટૂંક સમયમાં તમારો સંપર્ક કરીશું..!")){
+        location.reload(true);
+      }
+    })
+      .catch(error => console.error('Error!', error.message))
+  });
+ (jQuery);
